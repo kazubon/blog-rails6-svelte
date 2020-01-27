e0514caa06772cbe6ceaf58c8ceac004f5d70532
Kazuto Sato <kazubonx@gmail.com>
2020-01-27 14:54:38 +0900

    search-form.svelte, star.svelte

--- app/javascript/entries/search_form.svelte
+++ app/javascript/entries/search_form.svelte
@@ -1,5 +1,18 @@
 <script>
 export let query;
+
+let title = query.title;
+let tag = query.tag;
 </script>
 
-<h1>SearchForm</h1>
+<div>
+  <form action="/entries" method="get" class="form-inline mb-4">
+    <input type="text" name="title" class="form-control mr-3 mb-2"
+      bind:value={title} placeholder="タイトル">
+    <input type="text" name="tag" class="form-control mr-3 mb-2"
+      bind:value={tag} placeholder="タグ">
+    <input type="hidden" name="sort" value={query.sort || ''}>
+    <input type="hidden" name="user_id" value={query.user_id || ''}>
+    <button type="submit" class="btn btn-outline-primary mb-2">検索</button>
+  </form>
+</div>
--- app/javascript/entries/star.svelte
+++ app/javascript/entries/star.svelte
@@ -1,8 +1,33 @@
 <script>
+import Axios from 'axios';
+
 export let entryId;
 export let starCount;
 export let starrable;
 
+let count = starCount;
+
+function submit() {
+  Axios({
+    method: 'patch',
+    url: `/entries/${entryId}/star.json`,
+    headers: {
+      'X-CSRF-Token' : jQuery('meta[name="csrf-token"]').attr('content')
+    }
+  }).then((res) => {
+    count = res.data.count;
+  });
+}
 </script>
 
-<h1>Star</h1>
+<div class="text-right">
+  <big class="d-inline-block p-1 border rounded">
+    {#if starrable}
+      <a href="#" class="mr-1 ml-1 text-decoration-none"
+          on:click|preventDefault={submit} title="いいね">👍</a>
+    {/if}
+    {#if count > 0}
+      <span class="text-warning mr-1 ml-1">⭐️ {count}</span>
+    {/if}
+  </big>
+</div>
